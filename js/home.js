45c1b4bdceb9e5a0c632e2c7591fccb4a48ba2f8
jparrott06 <48459550+jparrott06@users.noreply.github.com>
2021-02-26 16:11:56 -0700

    fixed search icon

--- js/home.js
+++ js/home.js
@@ -3,7 +3,8 @@ function myFunction()
     document.getElementById('searchinput').style.backgroundColor = 'black';
     document.getElementById('box').style.backgroundColor = 'black';
     document.getElementById('box').style.border="2px solid #1DA1F2"
-    document.getElementById('icon').style.backgroundColor='black';
-    document.getElementById('icon').style.color='#1DA1F2';
+    document.getElementById('searchicon').style.backgroundColor='black';
+    document.getElementById('searchicon').style.color='#1DA1F2';
+    document.getElementById('searchicon_td').style.backgroundColor='black';
     return document;
 }
