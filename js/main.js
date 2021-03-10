a46166e7aad4a2ccae9e5d7f3f63f90818e9e55a
jparrott06 <48459550+jparrott06@users.noreply.github.com>
2021-03-10 16:21:30 -0700

    added next/prev button functionality: TODO: totalItems found changes bc it counts from new index

--- js/main.js
+++ js/main.js
@@ -3,11 +3,28 @@ const app = Vue.createApp(
         data() {
             return {
                 keyword: '',
-                result: null
+                result: null,
+                index: 0
             }
         },
         methods: {
             searchGoogleBooks(index) {
+
+                this.index = 0
+                fetch('https://www.googleapis.com/books/v1/volumes/?q=' + this.keyword + "&startIndex=" + index + "&maxResults=20")
+                    .then(response => response.json())
+                    .then(json => this.result = json)
+            },
+            NextsearchGoogleBooks(index) {
+
+                this.index += 20
+                fetch('https://www.googleapis.com/books/v1/volumes/?q=' + this.keyword + "&startIndex=" + index + "&maxResults=20")
+                    .then(response => response.json())
+                    .then(json => this.result = json)
+            },
+            PrevsearchGoogleBooks(index) {
+
+                this.index -=20
                 fetch('https://www.googleapis.com/books/v1/volumes/?q=' + this.keyword + "&startIndex=" + index + "&maxResults=20")
                     .then(response => response.json())
                     .then(json => this.result = json)
