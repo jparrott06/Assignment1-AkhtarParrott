d35c55757f5b54f3ad67e3dda58e2f17a8170226
jparrott06 <48459550+jparrott06@users.noreply.github.com>
2021-03-10 13:15:39 -0700

    converted authors array to string

--- js/components/bookDisplay.js
+++ js/components/bookDisplay.js
@@ -10,7 +10,7 @@ app.component('book-display', {
     /*html*/
     `<ul class="col-sm-12 col-md-6 col-xl-4">
     <a class = "list-group-item list-group-item-action active" :href ="this.bookObj.selfLink ">{{this.bookObj.volumeInfo.title}}</a>
-    <li class="list-group-item">{{this.bookObj.volumeInfo.authors}}</li>
+    <li class="list-group-item">{{this.bookObj.volumeInfo.authors.toString()}}</li>
     <li class = "list-group-item">{{this.bookObj.volumeInfo.publisher}}, {{this.bookObj.volumeInfo.publishedDate}}</li>
     <li class="list-group-item"><img class="thumbnail" src="{{ this.bookObj.volumeInfo.imageLinks.smallThumbnail }}"></li>
     <li class="list-group-item" v-if="this.bookObj.volumeInfo != null && this.bookObj.volumeInfo.averageRating != null">{{ this.bookObj.volumeInfo.pageCount }} pages, {{ this.bookObj.volumeInfo.averageRating }} <i class="fa fa-star"></i> rating</li>
