d222348280d2c5e4a49f965b4d2b25311c9810d8
jparrott06 <48459550+jparrott06@users.noreply.github.com>
2021-03-10 17:20:22 -0700

    fixed imageLinks for thumbnails

--- js/components/bookDisplay.js
+++ js/components/bookDisplay.js
@@ -12,7 +12,7 @@ app.component('book-display', {
     <a class = "list-group-item list-group-item-action active" :href ="this.bookObj.selfLink ">{{this.bookObj.volumeInfo.title}}</a>
     <li class="list-group-item" v-if="this.bookObj.volumeInfo.authors != undefined">{{this.bookObj.volumeInfo.authors.toString()}}</li>
     <li class = "list-group-item">{{this.bookObj.volumeInfo.publisher}}, {{this.bookObj.volumeInfo.publishedDate}}</li>
-    <li class="list-group-item"><img class="thumbnail" :src= "this.bookObj.volumeInfo.imageLinks.smallThumbnail"></li>
+    <li class="list-group-item" v-if="this.bookObj.volumeInfo.imageLinks != undefined"><img class="thumbnail" :src= "this.bookObj.volumeInfo.imageLinks.smallThumbnail"></li>
     <li class="list-group-item" v-if="this.bookObj.volumeInfo != null && this.bookObj.volumeInfo.averageRating != null">{{ this.bookObj.volumeInfo.pageCount }} pages, {{ this.bookObj.volumeInfo.averageRating }} <i class="fa fa-star"></i> rating</li>
     </ul>`,
     computed:
