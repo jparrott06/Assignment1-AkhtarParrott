c9b3ac77d03ef39baf75de39d7dc1ad831bebf39
Ayesha Akhtar <ayeshaakhtar13@gmail.com>
2021-03-10 22:38:29 -0700

    made changes to googlepg format

--- js/components/bookDisplay.js
+++ js/components/bookDisplay.js
@@ -8,12 +8,13 @@ app.component('book-display', {
     },
     template:
     /*html*/
-    `<ul class="col-sm-12 col-md-6 col-xl-4">
-    <a class = "list-group-item list-group-item-action active" :href ="this.bookObj.selfLink ">{{this.bookObj.volumeInfo.title}}</a>
-    <li class="list-group-item" v-if="this.bookObj.volumeInfo.authors != undefined">{{this.bookObj.volumeInfo.authors.toString()}}</li>
-    <li class = "list-group-item">{{this.bookObj.volumeInfo.publisher}}, {{this.bookObj.volumeInfo.publishedDate}}</li>
-    <li class="list-group-item" v-if="this.bookObj.volumeInfo.imageLinks != undefined"><img class="thumbnail" :src= "this.bookObj.volumeInfo.imageLinks.smallThumbnail"></li>
-    <li class="list-group-item" v-if="this.bookObj.volumeInfo != null && this.bookObj.volumeInfo.averageRating != null">{{ this.bookObj.volumeInfo.pageCount }} pages, {{ this.bookObj.volumeInfo.averageRating }} <i class="fa fa-star"></i> rating</li>
+    `<ul class="col-sm-12 col-md-6 col-xl-4 bookinfo">
+    <a class="list-group-item list-group-item-action active" :href ="this.bookObj.volumeInfo.infoLink " v-if="this.bookObj.volumeInfo.imageLinks != undefined"><img class="thumbnail" :src= "this.bookObj.volumeInfo.imageLinks.smallThumbnail"><br>{{this.bookObj.volumeInfo.title}}</a>
+    <li class="list-group-item active auth" v-if="this.bookObj.volumeInfo.authors != undefined">By: {{this.bookObj.volumeInfo.authors.toString()}}</li>
+    <li class = "list-group-item" v-if="this.bookObj.volumeInfo.publisher != undefined">Publisher: {{this.bookObj.volumeInfo.publisher}}</li>
+    <li class = "list-group-item" v-if="this.bookObj.volumeInfo.publishedDate != undefined">Published Date: {{this.bookObj.volumeInfo.publishedDate}}</li>
+    <li class="list-group-item" v-if="this.bookObj.volumeInfo.pageCount != undefined">{{ this.bookObj.volumeInfo.pageCount }} pages</li>
+    <li class="list-group-item" v-if="this.bookObj.volumeInfo.averageRating != null">{{ this.bookObj.volumeInfo.averageRating }} <i class="fa fa-star"></i> rating</li>
     </ul>`,
     computed:
     {
--- js/signup.js
+++ js/signup.js
@@ -4,3 +4,20 @@ function Verifyonclick() {
         location.href = "https://jparrott06.github.io/Assignment1-AkhtarParrott/verify.html";
     }
 }
+
+function ValidateForm()
+{
+    var pass1 = document.getElementById("pass1");
+    var pass2 = document.getElementById("pass2");
+    var errorr = document.querySelector("#passerror");
+    var valid = true;
+
+    if (pass1 != pass2)
+    {
+        errorr.classList.remove("invisible");
+        errorr.innerHTML = "The Date of Birth cannot be empty."
+        document.getElementById('txtdob').style.backgroundColor="yellow";
+        valid=false;
+    }
+    return valid;
+}
